438ca19729657beb102a39697a7480d4c308c114
oieduardorabelo <oieduardorabelo@gmail.com>
2019-11-13 23:43:17 +1300

    feat(mocha): with async/await

--- app/server.test.js
+++ app/server.test.js
@@ -3,12 +3,8 @@ import assert from 'assert';
 
 import { server } from "./server";
 
-it("GET /", () => {
-  return supertest(server)
-    .get("/")
-    .expect(200)
-    .then((res) => {
-      assert.equal(res.body.ok, true, "res.body.ok is `true`");
-      assert.equal(typeof res.body.uptime, 'number', "res.body.uptime is a `number`");
-    });
+it("GET /", async () => {
+  let res = await supertest(server).get("/").expect(200);
+  assert.equal(res.body.ok, true, "res.body.ok is `true`");
+  assert.equal(typeof res.body.uptime, 'number', "res.body.uptime is a `number`");
 });
