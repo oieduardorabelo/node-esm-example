e0e99a052db89d02f1a48afc7710728aec0b1b82
oieduardorabelo <oieduardorabelo@gmail.com>
2019-11-13 23:41:49 +1300

    feat(mocha): using promises

--- app/server.test.js
+++ app/server.test.js
@@ -3,16 +3,12 @@ import assert from 'assert';
 
 import { server } from "./server";
 
-it("GET /", (done) => {
-  supertest(server)
+it("GET /", () => {
+  return supertest(server)
     .get("/")
     .expect(200)
-    .end(function(err, res) {
-      if (err) throw err;
-
+    .then((res) => {
       assert.equal(res.body.ok, true, "res.body.ok is `true`");
       assert.equal(typeof res.body.uptime, 'number', "res.body.uptime is a `number`");
-
-      done()
     });
 });
